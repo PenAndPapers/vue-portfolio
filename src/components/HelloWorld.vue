09abd4d3d79ecf315d449863022b4b1653eee64e
steven <steven.pilagan@gmail.com>
2020-12-21 09:32:37 +0800

    main - initial project files and config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} {{test}}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -41,10 +41,13 @@ import { Options, Vue } from 'vue-class-component';
 @Options({
   props: {
     msg: String,
+    test: String,
   },
 })
+
 export default class HelloWorld extends Vue {
   msg!: string
+  test!: string
 }
 </script>
 
